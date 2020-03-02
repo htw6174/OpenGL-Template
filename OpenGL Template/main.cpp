bb371d89ab6e1e9fbe1a9fbc4498d5cd76800993
Cstough <32025644+Cstough@users.noreply.github.com>
2020-03-01 20:13:31 -0500

    initial commit Cody Branch
    init commit

--- OpenGL Template/main.cpp
+++ OpenGL Template/main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 void init(GLFWwindow* window) {}
 
 void display(GLFWwindow* window, double currentTime) {
-	glClearColor(1.0, 0.0, 0.0, 1.0);
+	glClearColor(0.0, 0.0, 0.0, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT);
 }
 
