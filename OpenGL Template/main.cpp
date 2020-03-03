ac5aa8045789180e49ce962b75f80034482f9db2
Hardy Whitney <htw6174@gmail.com>
2020-03-02 20:57:21 -0500

    chapter 2.2.5

--- OpenGL Template/main.cpp
+++ OpenGL Template/main.cpp
@@ -1,9 +1,41 @@
-#include "main.h"
 #include <GL\glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
 
 using namespace std;
+
+#define numVAOs 1
+
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+
+GLuint createShaderProgram() {
+	const char *vshaderSource =
+		"#version 430    \n"
+		"void main(void) \n"
+		"{ gl_Position = vec4(0.0, 0.0, 0.0, 1.0); }";
+
+	const char *fshaderSource =
+		"#version 430    \n"
+		"out vec4 color; \n"
+		"void main(void) \n"
+		"{ color = vec4(0.0, 0.0, 1.0, 1.0); }";
+
+	GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+	GLuint vfprogram = glCreateProgram();
+
+	glShaderSource(vShader, 1, &vshaderSource, NULL);
+	glShaderSource(fShader, 1, &fshaderSource, NULL);
+	glCompileShader(vShader);
+	glCompileShader(fShader);
+
+	glAttachShader(vfprogram, vShader);
+	glAttachShader(vfprogram, fShader);
+	glLinkProgram(vfprogram);
+
+	return vfprogram;
+}
 
 void init(GLFWwindow* window) {}
 
