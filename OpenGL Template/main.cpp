4fbb15fe00ec98bc36620f3123d4a066c95713c3
Lucas Rumney <lucasrumney94@gmail.com>
2020-03-11 12:50:52 -0700

    add up to program 2.2 split box

--- OpenGL Template/main.cpp
+++ OpenGL Template/main.cpp
@@ -5,11 +5,55 @@
 
 using namespace std;
 
-void init(GLFWwindow* window) {}
+#define numVAOs 1
+
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+
+GLuint createShaderProgram() {
+	const char* vshaderSource =
+		"#version 430 \n"
+		"void main(void) \n"
+		"{gl_Position = vec4(0.0,0.0,0.0,1.0);}";
+	const char* fshaderSource = 
+		"#version 430 \n"
+		"out vec4 color; \n"
+		"void main(void) \n"
+		"{ if (gl_FragCoord.x < 300) color=vec4(1.0, 0.0, 0.0, 1.0); else color=vec4(0.0, 0.0, 1.0, 1.0);\n}";
+	
+	GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+	glShaderSource(vShader, 1, &vshaderSource, NULL);
+	glShaderSource(fShader, 1, &fshaderSource, NULL);
+	glCompileShader(vShader);
+	glCompileShader(fShader);
+
+	GLuint vfProgram = glCreateProgram();
+	glAttachShader(vfProgram, vShader);
+	glAttachShader(vfProgram, fShader);
+	glLinkProgram(vfProgram);
+
+	return vfProgram;
+}
+
+void init(GLFWwindow* window) {
+	renderingProgram = createShaderProgram();
+	glGenVertexArrays(numVAOs, vao);
+	glBindVertexArray(vao[0]);
+
+}
 
 void display(GLFWwindow* window, double currentTime) {
-	glClearColor(1.0, 0.0, 0.0, 1.0);
+
+
+	glClearColor(0.0, 1.0, 0.0, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	glUseProgram(renderingProgram);
+	glPointSize(50.0f);
+	glDrawArrays(GL_POINTS, 0, 1);
+
 }
 
 int main(void) {
@@ -18,7 +62,7 @@ int main(void) {
 	}
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter 2 - program 1", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter 2 - program 2", NULL, NULL);
 	glfwMakeContextCurrent(window);
 	if (glewInit() != GLEW_OK) {
 		exit(EXIT_FAILURE);
