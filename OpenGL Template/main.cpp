06ffbe7332ca431ff0c526bd456a92b4c6135d08
Cstough <32025644+Cstough@users.noreply.github.com>
2020-03-28 19:11:28 -0400

    change
    copied from lucas branch

--- OpenGL Template/main.cpp
+++ OpenGL Template/main.cpp
@@ -1,7 +1,141 @@
-#include "Engine.h"
+#include <GL\glew.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <glm\glm.hpp>
+#include <glm\gtc\type_ptr.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+#include "Utils.h"
 
-int main(void) {
+using namespace std;
 
-	Engine engine;
-	engine.main();
+#define numVAOs 1
+#define numVBOs 2
+
+float cameraX, cameraY, cameraZ;
+float cubeLocX, cubeLocY, cubeLocZ;
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+GLuint vbo[numVBOs];
+
+// allocate variables used in display() to avoid allocating them every frame
+GLuint vLoc, projLoc, timeOffsetLoc;
+int width, height;
+float aspect, timeOffset;
+glm::mat4 pMat, vMat, mMat, tMat, rMat, mvMat;
+
+void setupVertices(void)
+{
+	// 2x2x2 cube at origin
+	float vertexPositions[108] =
+	{
+		-1.0f,  1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f, 1.0f,  1.0f, -1.0f, -1.0f,  1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f, 1.0f, -1.0f,  1.0f, 1.0f,  1.0f, -1.0f,
+		1.0f, -1.0f,  1.0f, 1.0f,  1.0f,  1.0f, 1.0f,  1.0f, -1.0f,
+		1.0f, -1.0f,  1.0f, -1.0f, -1.0f,  1.0f, 1.0f,  1.0f,  1.0f,
+		-1.0f, -1.0f,  1.0f, -1.0f,  1.0f,  1.0f, 1.0f,  1.0f,  1.0f,
+		-1.0f, -1.0f,  1.0f, -1.0f, -1.0f, -1.0f, -1.0f,  1.0f,  1.0f,
+		-1.0f, -1.0f, -1.0f, -1.0f,  1.0f, -1.0f, -1.0f,  1.0f,  1.0f,
+		-1.0f, -1.0f,  1.0f,  1.0f, -1.0f,  1.0f,  1.0f, -1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f, -100.0f, -1.0f, -1.0f, -1.0f, -1.0f,  1.0f,
+		-1.0f,  1.0f, -1.0f, 1.0f,  1.0f, -1.0f, 1.0f,  1.0f,  1.0f,
+		1.0f,  1.0f,  1.0f, -1.0f,  1.0f,  1.0f, -1.0f,  1.0f, -1.0f
+	};
+	glGenVertexArrays(1, vao);
+	glBindVertexArray(vao[0]);
+	glGenBuffers(numVBOs, vbo);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
+}
+
+
+void init(GLFWwindow* window)
+{
+	renderingProgram = Utils::createShaderProgram("vertShader.glsl", "fragShader.glsl");
+
+	glfwGetFramebufferSize(window, &width, &height);
+	aspect = (float)width / (float)height;
+	pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f);
+
+	cameraX = 0.0f; cameraY = 0.0f; cameraZ = 500.0f;
+	setupVertices();
+}
+
+void window_size_callback(GLFWwindow* win, int newWidth, int newHeight) {
+	aspect = (float)newWidth / (float)newHeight;
+	glViewport(0, 0, newWidth, newHeight);
+	pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f);
+}
+
+void display(GLFWwindow* window, double currentTime)
+{
+	glClear(GL_DEPTH_BUFFER_BIT);
+	glClearColor(0.0, 1.0, 0.0, 1.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glUseProgram(renderingProgram);
+
+	// get uniform variables for the v and projection matrices
+	vLoc = glGetUniformLocation(renderingProgram, "v_matrix");
+	projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
+
+	// build view matrix, model matrix, then MV matrix
+	vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
+
+	// Copy Perspective and V matrices to the Uniform Variables
+	glUniformMatrix4fv(vLoc, 1, GL_FALSE, glm::value_ptr(vMat));
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+
+	timeOffset = (float)currentTime;
+	timeOffsetLoc = glGetUniformLocation(renderingProgram, "time_offset");
+	glUniform1f(timeOffsetLoc, (float)timeOffset);
+
+	// Associate VBO with the vertex attribute in the vertex shader
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+
+	// adjust OpenGL Settings and draw model
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+
+	glDrawArraysInstanced(GL_TRIANGLES, 0, 36, 100000);
+}
+
+int main(void)
+{
+	cout << "Hello World!" << endl;
+	if (!glfwInit()) {
+		exit(EXIT_FAILURE);
+	}
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	GLFWwindow* window = glfwCreateWindow(600, 600, "CUBE!", NULL, NULL);
+	glfwMakeContextCurrent(window);
+	if (glewInit() != GLEW_OK) {
+		exit(EXIT_FAILURE);
+	}
+	glfwSwapInterval(1);
+
+	glfwSetWindowSizeCallback(window, window_size_callback);
+
+	init(window);
+
+	Utils::checkOpenGLError();
+	Utils::printShaderLog(renderingProgram);
+	Utils::printProgramLog(renderingProgram);
+
+
+	while (!glfwWindowShouldClose(window)) {
+		display(window, glfwGetTime());
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
 }
