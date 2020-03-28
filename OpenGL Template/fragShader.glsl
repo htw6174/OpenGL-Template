368276e9dbd2c26149e576004dada4235d85313c
Lucas Rumney <lucasrumney94@gmail.com>
2020-03-28 12:35:16 -0700

    failing, attempt to get box to appear

--- OpenGL Template/fragShader.glsl
+++ OpenGL Template/fragShader.glsl
@@ -1,13 +1,11 @@
 #version 430
+
 out vec4 color;
+
+uniform mat4 mv_matrix;
+uniform mat4 proj_matrix;
+
 void main(void)
 { 
-	if (gl_FragCoord.x < 300) 
-	{ 
-		color=vec4(1.0, 0.0, 0.0, 1.0); 
-	}
-	else 
-	{
-		color=vec4(0.0, 0.0, 1.0, 1.0);
-	}
+	color = vec4(1.0, 0.0, 0.0, 1.0);
 }
--- OpenGL Template/main.cpp
+++ OpenGL Template/main.cpp
@@ -1,77 +1,65 @@
 #include "main.h"
-#include "Utils.h"
 #include <GL\glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <fstream>
+#include <cmath>
+#include <glm\glm.hpp>
+#include <glm\gtc\type_ptr.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+#include "Utils.h"
 
 using namespace std;
 
 #define numVAOs 1
+#define numVBOs 2
 
+float cameraX, cameraY, cameraZ;
+float cubeLocX, cubeLocY, cubeLocZ;
 GLuint renderingProgram;
 GLuint vao[numVAOs];
-float x = 0.0f;
-float inc = 0.01f;
+GLuint vbo[numVBOs];
 
+// allocate variables used in display() to avoid allocating them every frame
+GLuint mvLoc, projLoc;
+int width, height;
+float aspect;
+glm::mat4 pMat, vMat, mMat, mvMat;
 
-GLuint createShaderProgram() 
+void setupVertices(void) 
 {
-	GLint vertCompiled;
-	GLint fragCompiled;
-	GLint linked;
-
-	string vertShaderString = Utils::readShaderFile("vertShader.glsl");
-	string fragShaderString = Utils::readShaderFile("fragShader.glsl");
-
-	const char* vertShaderSource = vertShaderString.c_str();
-		
-	const char* fragShaderSource = fragShaderString.c_str();
-
-	// Catch Errors while compiling Shaders
-	GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vShader, 1, &vertShaderSource, NULL);
-	glCompileShader(vShader);
-	Utils::checkOpenGLError();
-	glGetShaderiv(vShader, GL_COMPILE_STATUS, &vertCompiled);
-	if (vertCompiled != 1) 
+	// 2x2x2 cube at origin
+	float vertexPositions[108] = 
 	{
-		cout << "Vertex Compilation Failed!" << endl;
-		Utils::printShaderLog(vShader);
-	}
+		-1.0f,  1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f, 1.0f,  1.0f, -1.0f, -1.0f,  1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f, 1.0f, -1.0f,  1.0f, 1.0f,  1.0f, -1.0f,
+		1.0f, -1.0f,  1.0f, 1.0f,  1.0f,  1.0f, 1.0f,  1.0f, -1.0f,
+		1.0f, -1.0f,  1.0f, -1.0f, -1.0f,  1.0f, 1.0f,  1.0f,  1.0f,
+		-1.0f, -1.0f,  1.0f, -1.0f,  1.0f,  1.0f, 1.0f,  1.0f,  1.0f,
+		-1.0f, -1.0f,  1.0f, -1.0f, -1.0f, -1.0f, -1.0f,  1.0f,  1.0f,
+		-1.0f, -1.0f, -1.0f, -1.0f,  1.0f, -1.0f, -1.0f,  1.0f,  1.0f,
+		-1.0f, -1.0f,  1.0f,  1.0f, -1.0f,  1.0f,  1.0f, -1.0f, -1.0f,
+		1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f,  1.0f,
+		-1.0f,  1.0f, -1.0f, 1.0f,  1.0f, -1.0f, 1.0f,  1.0f,  1.0f,
+		1.0f,  1.0f,  1.0f, -1.0f,  1.0f,  1.0f, -1.0f,  1.0f, -1.0f
+	};
+	glGenVertexArrays(1, vao);
+	glBindVertexArray(vao[0]);
+	glGenBuffers(numVBOs, vbo);
 
-	GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fShader, 1, &fragShaderSource, NULL);
-	glCompileShader(fShader);
-	Utils::checkOpenGLError();
-	glGetShaderiv(fShader, GL_COMPILE_STATUS, &fragCompiled);
-	if (fragCompiled != 1) 
-	{
-		cout << "Fragment Compilation Failed!" << endl;
-		Utils::printShaderLog(fShader);
-	}
-
-	// Catch Errors while linking Shaders
-	GLuint vfProgram = glCreateProgram();
-	glAttachShader(vfProgram, vShader);
-	glAttachShader(vfProgram, fShader);
-	glLinkProgram(vfProgram);
-	Utils::checkOpenGLError();
-
-	glGetProgramiv(vfProgram, GL_LINK_STATUS, &linked);
-	if (linked != 1) 
-	{
-		cout << "Linking Failed!" << endl;
-		Utils::printProgramLog(vfProgram);
-	}
-	return vfProgram;
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
 }
+
 
 void init(GLFWwindow* window) 
 {
-	renderingProgram = createShaderProgram();
-	glGenVertexArrays(numVAOs, vao);
-	glBindVertexArray(vao[0]);
-
+	renderingProgram = Utils::createShaderProgram("vertShader.glsl", "fragShader.glsl");
+	cameraX = 0.0f; cameraY = 0.0f; cameraZ = 0.0f;
+	cubeLocX = 0.0f; cubeLocY = -2.0f; cubeLocZ = 0.0f;
+	
+	setupVertices();
 }
 
 void display(GLFWwindow* window, double currentTime) 
@@ -82,18 +70,39 @@ void display(GLFWwindow* window, double currentTime)
 
 	glUseProgram(renderingProgram);
 
-	x += inc; // move triangle along x
-	if (x > 1.0f) inc = -0.01f;
-	if (x < -1.0f) inc = 0.01f;
+	// get uniform variables for the mv and projection matrices
+	mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
+	projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
 
-	GLuint offsetLoc = glGetUniformLocation(renderingProgram, "offset"); // get ptr to 'offset' uniform variable in the vert shader
-	glProgramUniform1f(renderingProgram, offsetLoc, x);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+	// build perspective matrix
+	glfwGetFramebufferSize(window, &width, &height);
+	aspect = (float)width / (float)height;
+	pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f); // 1.0472 is 60 degrees vertical FOV
+
+	// build view matrix, model matrix, then MV matrix
+	vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
+	mMat = glm::translate(glm::mat4(1.0f), glm::vec3(cubeLocX, cubeLocY, cubeLocZ));
+	mvMat = vMat * mMat;
+
+	// Copy Perspective and MV matrices to the Uniform Variables
+	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+
+	// Associate VBO with the vertex attribute in the vertex shader
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+	
+	// adjust OpenGL Settings and draw model
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
 
 }
 
 int main(void) 
 {
+
 	cout << "Hello World!" << endl;
 	if (!glfwInit()) {
 		exit(EXIT_FAILURE);
