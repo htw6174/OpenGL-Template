530b0d1da58c154f8e4927307f261f2d5e0c8b44
Lucas Rumney <lucasrumney94@gmail.com>
2020-03-29 13:29:43 -0700

    working! cube and pyramid!

--- OpenGL Template/main.cpp
+++ OpenGL Template/main.cpp
@@ -16,12 +16,13 @@ using namespace std;
 
 float cameraX, cameraY, cameraZ;
 float cubeLocX, cubeLocY, cubeLocZ;
+float pyrLocX, pyrLocY, pyrLocZ;
 GLuint renderingProgram;
 GLuint vao[numVAOs];
 GLuint vbo[numVBOs];
 
 // allocate variables used in display() to avoid allocating them every frame
-GLuint vLoc, projLoc, timeOffsetLoc;
+GLuint mvLoc, projLoc, timeOffsetLoc;
 int width, height;
 float aspect, timeOffset;
 glm::mat4 pMat, vMat, mMat, tMat, rMat, mvMat;
@@ -44,12 +45,28 @@ void setupVertices(void)
 		-1.0f,  1.0f, -1.0f, 1.0f,  1.0f, -1.0f, 1.0f,  1.0f,  1.0f,
 		1.0f,  1.0f,  1.0f, -1.0f,  1.0f,  1.0f, -1.0f,  1.0f, -1.0f
 	};
+
+	//Pyramid with 18 vertices comprising 6 triangles (4 tri sides + 2 tri on bottom)
+	float pyramidPositions[54] =
+	{ -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 0.0f, 1.0f, 0.0f,    //front
+		1.0f, -1.0f, 1.0f, 1.0f, -1.0f, -1.0f, 0.0f, 1.0f, 0.0f,    //right
+		1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 0.0f, 1.0f, 0.0f,  //back
+		-1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, 0.0f, 1.0f, 0.0f,  //left
+		-1.0f, -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f, //LF
+		1.0f, -1.0f, 1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f  //RR
+	};
+
+
+
 	glGenVertexArrays(1, vao);
 	glBindVertexArray(vao[0]);
 	glGenBuffers(numVBOs, vbo);
 
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexPositions), vertexPositions, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(pyramidPositions), pyramidPositions, GL_STATIC_DRAW);
 }
 
 
@@ -61,7 +78,10 @@ void init(GLFWwindow* window)
 	aspect = (float)width / (float)height;
 	pMat = glm::perspective(1.0472f, aspect, 0.1f, 1000.0f);
 
-	cameraX = 0.0f; cameraY = 0.0f; cameraZ = 500.0f;
+	cameraX = -2.0f; cameraY = -1.0f; cameraZ = 8.0f;
+	cubeLocX = 0.0f; cubeLocY = -2.0f; cubeLocZ = 0.0f;
+	pyrLocX = 0.0f; pyrLocY = 2.0f; pyrLocZ = 0.0f;
+
 	setupVertices();
 }
 
@@ -80,19 +100,20 @@ void display(GLFWwindow* window, double currentTime)
 	glUseProgram(renderingProgram);
 
 	// get uniform variables for the v and projection matrices
-	vLoc = glGetUniformLocation(renderingProgram, "v_matrix");
+	mvLoc = glGetUniformLocation(renderingProgram, "mv_matrix");
 	projLoc = glGetUniformLocation(renderingProgram, "proj_matrix");
 
 	// build view matrix, model matrix, then MV matrix
 	vMat = glm::translate(glm::mat4(1.0f), glm::vec3(-cameraX, -cameraY, -cameraZ));
 
-	// Copy Perspective and V matrices to the Uniform Variables
-	glUniformMatrix4fv(vLoc, 1, GL_FALSE, glm::value_ptr(vMat));
-	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
 
-	timeOffset = (float)currentTime;
-	timeOffsetLoc = glGetUniformLocation(renderingProgram, "time_offset");
-	glUniform1f(timeOffsetLoc, (float)timeOffset);
+	// draw the cube using buffer 0
+	mMat = glm::translate(glm::mat4(1.0), glm::vec3(cubeLocX, cubeLocY, cubeLocZ));
+	mvMat = vMat * mMat;
+
+	// Copy Perspective and V matrices to the Uniform Variables
+	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
 
 	// Associate VBO with the vertex attribute in the vertex shader
 	glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
@@ -103,7 +124,24 @@ void display(GLFWwindow* window, double currentTime)
 	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LEQUAL);
 
-	glDrawArraysInstanced(GL_TRIANGLES, 0, 36, 100000);
+	glDrawArrays(GL_TRIANGLES, 0, 36);
+
+	// draw pyramid using buffer 1
+	mMat = glm::translate(glm::mat4(1.0f), glm::vec3(pyrLocX, pyrLocY, pyrLocZ));
+	mvMat = vMat * mMat;
+
+	glUniformMatrix4fv(mvLoc, 1, GL_FALSE, glm::value_ptr(mvMat));
+	glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(pMat));
+
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LEQUAL);
+
+	glDrawArrays(GL_TRIANGLES, 0, 18);
+
 }
 
 int main(void) 
@@ -114,7 +152,7 @@ int main(void)
 	}
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	GLFWwindow* window = glfwCreateWindow(600, 600, "CUBE!", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(600, 600, "CUBE AND PYRAMID!", NULL, NULL);
 	glfwMakeContextCurrent(window);
 	if (glewInit() != GLEW_OK) {
 		exit(EXIT_FAILURE);
--- OpenGL Template/vertShader.glsl
+++ OpenGL Template/vertShader.glsl
@@ -2,9 +2,8 @@
 
 layout (location=0) in vec3 position;
 
-uniform mat4 v_matrix;
+uniform mat4 mv_matrix;
 uniform mat4 proj_matrix;
-uniform float time_offset;
 
 out vec4 varyingColor;
 
@@ -15,23 +14,9 @@ mat4 buildTranslate(float x, float y, float z);
 
 void main(void)
 {
-	float i = gl_InstanceID + time_offset;
-	float a = sin(203.0 * i/8000.0) * 403.0;	//when 100000 instances
-	float b = cos(301.0 * i/4001.0) * 401.0;
-	float c = sin(400.0 * i/6003.0) * 405.0;
-	
-	// build the rotation and translation matrices to be applied to this instance
-	mat4 localRotX = buildRotateX(1.75*i);
-	mat4 localRotY = buildRotateY(1.75*i);
-	mat4 localRotZ = buildRotateZ(1.75*i);
-	mat4 localTrans = buildTranslate(a, b, c);
-
-	// build the model and model view matrices
-	mat4 newM_matrix = localTrans * localRotX * localRotY * localRotZ;
-	mat4 mv_matrix = v_matrix * newM_matrix;
-
 	gl_Position = proj_matrix * mv_matrix * vec4(position, 1.0);
-	varyingColor = vec4(position, 1.0) * 0.5 + vec4(0.5, 0.5, 0.5, 0.5);
+	
+	varyingColor = vec4(position,1.0)*0.5 + vec4(0.5, 0.5, 0.5, 0.5);
 }
 
 mat4 buildTranslate(float x, float y, float z)
