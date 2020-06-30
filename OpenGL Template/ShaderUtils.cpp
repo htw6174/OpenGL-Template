1638982d87ae7baf5802edddbccc6c5b0dbbdb95
Lucas Rumney <lucasrumney94@gmail.com>
2020-06-29 21:32:48 -0700

    Fix name of geometry shaders and vc project files (#11)
    Co-authored-by: Hardy Whitney <htw6174@gmail.com>

--- OpenGL Template/ShaderUtils.cpp
+++ OpenGL Template/ShaderUtils.cpp
@@ -4,8 +4,8 @@ std::map<std::string, GLuint> ShaderUtils::ShaderMap;
 
 void ShaderUtils::LoadAllShaders()
 {
-	ShaderMap.insert(std::make_pair("Player", Utils::createShaderProgram("default.vert", "player.geo", "player.frag")));
-	ShaderMap.insert(std::make_pair("Asteroid", Utils::createShaderProgram("asteroid.vert", "asteroid.geo", "asteroid.frag")));
+	ShaderMap.insert(std::make_pair("Player", Utils::createShaderProgram("default.vert", "player.geom", "player.frag")));
+	ShaderMap.insert(std::make_pair("Asteroid", Utils::createShaderProgram("asteroid.vert", "asteroid.geom", "asteroid.frag")));
 	ShaderMap.insert(std::make_pair("Bullet", Utils::createShaderProgram("default.vert", "bullet.frag")));
 	ShaderMap.insert(std::make_pair("Plane", Utils::createShaderProgram("default.vert", "default.frag")));
 }
